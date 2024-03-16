ca8d04a9d6260d48063166ad9952bbcfc972fd8f
Rustam <tabarzin@gmail.com>
2024-03-16 17:04:55 +0300

    refactor: refactor review

--- eatly/src/components/organisms/Review/Review.tsx
+++ eatly/src/components/organisms/Review/Review.tsx
@@ -8,34 +8,36 @@ const Review = () => {
   return (
     <>
       <div className={st.review}>
-        <Text type={"h3"}>
+        <Text className={st.h3} type={"h3"}>
           <span className={st.highlight}>Customer</span> Say
         </Text>
-      </div>
-      <div className={st.reviewcards_wrapper}>
-        <div>
-          <ReviewCard
-            isSmall={false}
-            reviewerName={"@omottley2h"}
-            commentary={"I cannot believe how I found you, this is so pretty."}
-          />
-        </div>
-        <div className={st.small_review_cards_block}>
-          <div className={st.small_review_cards}>
+        <div className={st.reviewcards_wrapper}>
+          <div>
             <ReviewCard
-              isSmall={true}
-              commentary={
-                "I cannot believe how I found you, this is so pretty."
-              }
-            />
-            <ReviewCard
-              isSmall={true}
+              isSmall={false}
+              reviewerName={"@omottley2h"}
               commentary={
                 "I cannot believe how I found you, this is so pretty."
               }
             />
           </div>
-          <ReviewCardsScrollBar />
+          <div className={st.small_review_cards_block}>
+            <div className={st.small_review_cards}>
+              <ReviewCard
+                isSmall={true}
+                commentary={
+                  "I cannot believe how I found you, this is so pretty."
+                }
+              />
+              <ReviewCard
+                isSmall={true}
+                commentary={
+                  "I cannot believe how I found you, this is so pretty."
+                }
+              />
+            </div>
+            <ReviewCardsScrollBar />
+          </div>
         </div>
       </div>
     </>
