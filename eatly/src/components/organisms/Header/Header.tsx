5acc93c9600156a0729bd23e3a0735dd2351fcea
Rustam <tabarzin@gmail.com>
2024-03-20 20:54:28 +0300

    feat: add clickable Logo

--- eatly/src/components/organisms/Header/Header.tsx
+++ eatly/src/components/organisms/Header/Header.tsx
@@ -1,11 +1,14 @@
 import st from "./header.module.css";
 import NavMenu from "../../molecules/NavMenu/NavMenu";
 import Logo from "../../atoms/Logo/Logo";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className={st.header}>
-      <Logo />
+      <Link className={st.link} to="/">
+        <Logo />
+      </Link>
 
       <NavMenu />
     </div>
