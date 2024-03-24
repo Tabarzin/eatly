a7ce2812f9b51bec41f5b3825f17bb4ac441996c
Rustam <tabarzin@gmail.com>
2024-03-24 13:32:11 +0300

    feat: add slider

--- eatly/src/components/organisms/Review/Review.tsx
+++ eatly/src/components/organisms/Review/Review.tsx
@@ -1,27 +1,29 @@
 import st from "./review.module.css";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import Text from "../../atoms/Text/Text";
 import ReviewCard from "../../molecules/ReviewCard/ReviewCard";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchComments,
-  Comment,
-  CommentsState,
-} from "../../../store/commentsSlice";
+import { fetchComments, CommentsState } from "../../../store/commentsSlice";
 import { RootState } from "../../../store/store";
+import useEmblaCarousel from "embla-carousel-react";
 
 const Review: React.FC = () => {
   const dispatch = useDispatch();
-
   const { comments, status, error } = useSelector<RootState, CommentsState>(
     (state) => state.comments
   );
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   useEffect(() => {
     dispatch(fetchComments());
   }, [dispatch]);
-
-  const first3Comments = comments.slice(0, 3);
 
   if (status === "loading") {
     return <div>Loading...</div>;
@@ -31,6 +33,7 @@ const Review: React.FC = () => {
     return <div>Error: {error}</div>;
   }
 
+  const first6Comments = comments.slice(0, 6);
   return (
     <>
       <div className={st.review}>
@@ -38,13 +41,22 @@ const Review: React.FC = () => {
           <span className={st.highlight}>Customer</span> Say
         </Text>
         <div className={st.comments_block}>
-          {first3Comments.map((comment) => (
-            <ReviewCard
-              key={comment.id}
-              reviewerName={`@${comment.user.username}`}
-              commentary={comment.body}
-            />
-          ))}
+          <div className={st.embla} ref={emblaRef}>
+            <div className={st.embla__container}>
+              {first6Comments.map((comment) => (
+                <div className={st.embla__slide} key={comment.id}>
+                  <ReviewCard
+                    reviewerName={`@${comment.user.username}`}
+                    commentary={comment.body}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={st.embla_btns}>
+            <button className={st.dot} onClick={scrollPrev}></button>
+            <button className={st.dot} onClick={scrollNext}></button>
+          </div>
         </div>
       </div>
     </>
