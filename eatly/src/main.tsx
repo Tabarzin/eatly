bb7609bcb34305e91d1b460e4bd0790bceef80ae
Rustam <tabarzin@gmail.com>
2024-03-31 15:13:34 +0300

    feat: add hover to blog card

--- eatly/src/main.tsx
+++ eatly/src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import st from "./index.module.css";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/store";
 import { Provider } from "react-redux";
+import "./index.module.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
