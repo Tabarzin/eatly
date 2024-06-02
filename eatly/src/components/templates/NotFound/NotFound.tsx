5f0022699eda2b0d28a02a1fb8fe9be402f0b8ac
Rustam <tabarzin@gmail.com>
2024-06-02 15:47:07 +0300

    feat: add header to 404 page

--- eatly/src/components/templates/NotFound/NotFound.tsx
+++ eatly/src/components/templates/NotFound/NotFound.tsx
@@ -3,18 +3,22 @@ import st from "./notfound.module.css";
 import oops from "../../../assets/oops.svg";
 import { HashLink as Link } from "react-router-hash-link";
 import React from "react";
+import Header from "../../organisms/Header/Header";
 
 const NotFound = () => {
   return (
-    <main className={st.notfound}>
-      <img src={oops} alt="Page Not Found" className={st.notfound_img} />
-      <Link to="/">
-        <Button type={"primary"}>Go Home</Button>
-      </Link>
-      <span className={st.notfound_copyright}>
-        Image by <a href="http://www.freepik.com"> Freepik </a>{" "}
-      </span>
-    </main>
+    <>
+      <Header />
+      <main className={st.notfound}>
+        <img src={oops} alt="Page Not Found" className={st.notfound_img} />
+        <Link to="/">
+          <Button type={"primary"}>Go Home</Button>
+        </Link>
+        <span className={st.notfound_copyright}>
+          Image by <a href="http://www.freepik.com"> Freepik </a>{" "}
+        </span>
+      </main>
+    </>
   );
 };
 
